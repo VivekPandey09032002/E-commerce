f15200fc01d70afa7d9c5a9400216d0f3ef57e44
VivekPandey09032002 <vivekpandeykcc09032002@gmail.com>
2022-12-08 17:48:06 +0530

    hole and soul

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,13 +11,7 @@ import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="/about" element={<About />}></Route>
-        <Route exact path="/contact" element={<Contact />}></Route>
-      </Routes>
-      <Footer/>
+
     </div>
   );
 }
